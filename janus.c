d3787be655bfbc95c91727653decb13769f35ad8
Pierce Lopez <pierce.lopez@gmail.com>
2014-12-02 13:02:20 -0500

    logging line spelling error: RammitMQ -> RabbitMQ

--- janus.c
+++ janus.c
@@ -4190,7 +4190,7 @@ gint main(int argc, char *argv[])
 #else
 	item = janus_config_get_item_drilldown(config, "rabbitmq", "enable");
 	if(!item || !item->value || !janus_is_true(item->value)) {
-		JANUS_LOG(LOG_WARN, "RammitMQ support disabled\n");
+		JANUS_LOG(LOG_WARN, "RabbitMQ support disabled\n");
 	} else {
 		/* Parse configuration */
 		const char *rmqhost = "localhost";
