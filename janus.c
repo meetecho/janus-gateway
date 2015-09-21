cdeda3390aa976f36ebd7337daf8b9a5b719c0bd
Lorenzo Miniero <lminiero@gmail.com>
2015-09-21 15:05:43 +0200

    Fixed missing mutex unlock

--- janus.c
+++ janus.c
@@ -1737,6 +1737,7 @@ int janus_process_incoming_request(janus_request_source *source, json_t *root) {
 			/* We got multiple candidates in an array */
 			if(!json_is_array(candidates)) {
 				ret = janus_process_error(source, session_id, transaction_text, JANUS_ERROR_INVALID_ELEMENT_TYPE, "candidates is not an array");
+				janus_mutex_unlock(&handle->mutex);
 				goto jsondone;
 			}
 			JANUS_LOG(LOG_INFO, "Got multiple candidates (%zu)\n", json_array_size(candidates));
