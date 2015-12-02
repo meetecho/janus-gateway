de24ab3da0d65b8a3f297a19df0ba35dd4a17668
Lorenzo Miniero <lminiero@gmail.com>
2015-12-02 13:15:38 +0100

    Only modify the ice-udp and ice-tcp libnice attributes if the library supports them

--- ice.c
+++ ice.c
@@ -2379,8 +2379,10 @@ int janus_ice_setup_local(janus_ice_handle *handle, int offer, int audio, int vi
 		"main-context", handle->icectx,
 		"reliable", FALSE,
 		"full-mode", janus_ice_lite_enabled ? FALSE : TRUE,
+#ifdef HAVE_LIBNICE_TCP
 		"ice-udp", TRUE,
 		"ice-tcp", janus_ice_tcp_enabled ? TRUE : FALSE,
+#endif
 		NULL);
 	handle->agent_created = janus_get_monotonic_time();
 	/* Any STUN server to use? */
