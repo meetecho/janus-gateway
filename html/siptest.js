af7c2838999693e37d2decfc9fa36be0f50ddcac
Filip Jenicek <fjenicek@kerio.com>
2016-02-03 15:28:19 +0100

    Send DTMF tones using SIP INFO messages - use inband in the demo

--- html/siptest.js
+++ html/siptest.js
@@ -312,12 +312,11 @@ $(document).ready(function() {
 												$('#dtmf').append('<button class="btn btn-info dtmf">*</button>');
 										}
 										$('.dtmf').click(function() {
-											// Send DTMF tone
-											// Inband
-											// sipcall.dtmf({dtmf: { tones: $(this).text()}});
+											// Send DTMF tone (inband)
+											sipcall.dtmf({dtmf: { tones: $(this).text()}});
 
-											// Use SIP INFO messages
-											sipcall.send({"message": {"request": "dtmf_info", "digit": $(this).text()}});
+											// You can also send DTMF tones using SIP INFO
+											// sipcall.send({"message": {"request": "dtmf_info", "digit": $(this).text()}});
 										});
 									}
 									// Show the peer and hide the spinner when we get a playing event
