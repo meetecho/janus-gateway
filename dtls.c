6cc644565c03b7f467fb1ff0cb491c561d73129d
Lorenzo Miniero <lminiero@gmail.com>
2016-05-28 14:23:11 +0200

    Fixed typo

--- dtls.c
+++ dtls.c
@@ -87,7 +87,7 @@ const gchar *janus_get_dtls_srtp_role(janus_dtls_role role) {
 
 /* Helper to notify DTLS state changes to the event handlers */
 static void janus_dtls_notify_state_change(janus_dtls_srtp *dtls) {
-	if(janus_events_is_enabled())
+	if(!janus_events_is_enabled())
 		return;
 	if(dtls == NULL)
 		return;
