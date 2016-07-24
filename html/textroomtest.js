5f5b85934fbf2cbc198a3f8ee075ca1f44e98d86
joshdickson40 <josh.dickson@wpi.edu>
2016-07-24 15:17:13 -0400

    reset tabs

--- html/textroomtest.js
+++ html/textroomtest.js
@@ -337,8 +337,8 @@ function sendData() {
 		textroom: "message",
 		transaction: randomString(12),
 		room: myroom,
-        text: data,
-        ack: true // acknowledge message reciept by server? (does NOT acknowledge receipt by other clients)
+ 		text: data,
+ 		ack: true // acknowledge message reciept by server? (does NOT acknowledge receipt by other clients)
 	};
 	textroom.data({
 		text: JSON.stringify(message),
