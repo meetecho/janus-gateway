380e2290e633fe392af8682509f5f5c4619c76b3
Emmanuel Riou <emmanuel.riou@dazzl.tv>
2017-09-27 12:24:36 +0200

    [skip ci] add travis.sh helper script for .travis.yml with coverity_scan

---
+++ travis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+janus_configure() {
+  sh autogen.sh                                                                          
+  export CPPFLAGS="-I${HOME}/install/srtp/include -I${HOME}/install/boringssl/include -I${HOME}/install/usrsctp/include -I${HOME}/install/rabbit/include"
+  export LDFLAGS="-L${HOME}/install/srtp/lib -L${HOME}/install/boringssl/lib -L${HOME}/install/usrsctp/lib -L${HOME}/install/rabbit/lib"
+  ./configure --enable-boringssl="${HOME}/install/boringssl" --disable-all-plugins
+}
+
+janus_build() {
+  make
+}
+
+janus_all() {
+  janus_configure
+  janus_build
+}
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 {janus_configure|janus_build|janus_all}"
+  exit 1
+fi
+
+eval "$1"
