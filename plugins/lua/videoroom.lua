17cc31819aaeca72bb2651098ded7c56a1169735
Lorenzo Miniero <lminiero@gmail.com>
2017-11-03 17:07:19 +0100

    Use ipairs instead of pairs when iterating on pending coroutines

--- plugins/lua/videoroom.lua
+++ plugins/lua/videoroom.lua
@@ -662,7 +662,7 @@ function resumeScheduler()
 	-- you're free not to use this and just return, but the C Lua plugin
 	-- expects this method to exist so it MUST be present, even if empty
 	logger.print("Resuming coroutines")
-	for index,task in pairs(tasks) do
+	for index,task in ipairs(tasks) do
 		local success, result = pcall(coroutine.resume(task.co, task.id, task.tr, task.msg, task.jsep))
 		if not success then
 			logger.print(colors("[%{red}exception%{reset}]") .. " " .. dumpTable(result))
