cd3c132cde6a4d493514701f7418347c06ad513a
Lorenzo Miniero <lminiero@gmail.com>
2018-05-22 12:05:32 +0200

    Take into account overridable extension property when extending dependencies

--- html/janus.js
+++ html/janus.js
@@ -248,7 +248,7 @@ Janus.init = function(options) {
 		Janus.log("Initializing library");
 
 		var usedDependencies = null;
-		if(!options.dependencies || !options.dependencies.isArray ||
+		if(!options.dependencies || !options.dependencies.isArray || !options.dependencies.extension ||
 				!options.dependencies.webRTCAdapter || !options.dependencies.httpAPICall ||
 				!options.dependencies.checkJanusExtension || options.dependencies.newWebSocket) {
 			// Not all dependencies have been overridden: use the default ones as a basis
