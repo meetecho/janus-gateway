a04f79f33e96f51a8fb18aab7aa7d19859ac9eaf
Lorenzo Miniero <lminiero@gmail.com>
2018-06-19 10:57:55 +0200

    Make sure we only call g_main_loop_quit once (see #1276)

--- janus.c
+++ janus.c
@@ -2171,6 +2171,7 @@ int janus_process_incoming_admin_request(janus_request *request) {
 			json_object_set_new(info, "agent-created", json_integer(handle->agent_created));
 			json_object_set_new(info, "ice-mode", json_string(janus_ice_is_ice_lite_enabled() ? "lite" : "full"));
 			json_object_set_new(info, "ice-role", json_string(handle->controlling ? "controlling" : "controlled"));
+			json_object_set_new(info, "ice-loop-running", g_atomic_int_get(&handle->looprunning) ? json_true() : json_false());
 		}
 		json_t *sdps = json_object();
 		if(handle->rtp_profile)
