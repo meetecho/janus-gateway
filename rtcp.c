6ca8f4c458bccc7610e08c405160525f33cd255d
Yongje Lee <yongje.lee@hpcnt.com>
2018-07-28 08:35:19 +0900

    add padding for sdes cname
    sdes chunk should end with null terminator
    
    see: https://tools.ietf.org/html/rfc1889
    6.4 SDES: Source description RTCP packet
     Items are contiguous, i.e., items are not individually padded to a
    32-bit boundary. Text is not null terminated because some multi-octet
    encodings include null octets. The list of items in each chunk is
    terminated by one or more null octets, the first of which is
    interpreted as an item type of zero to denote the end of the list,
    and the remainder as needed to pad until the next 32-bit boundary. A
    chunk with zero items (four null octets) is valid but useless.

--- rtcp.c
+++ rtcp.c
@@ -993,7 +993,7 @@ int janus_rtcp_sdes_cname(char *packet, int len, const char *cname, int cnamelen
 	rtcp->rc = 1;
 	int plen = 8;	/* Header + chunk + item header */
 	plen += cnamelen+2;
-	if((cnamelen+2)%4)	/* Account for padding */
+	if((cnamelen+3)%4)	/* Account for padding */
 		plen += 4;
 	if(len < plen) {
 		JANUS_LOG(LOG_ERR, "Buffer too small for SDES message: %d < %d\n", len, plen);
