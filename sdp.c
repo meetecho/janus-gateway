0a5f379ad3bc521718cd0fb90e3008a09837baff
Lorenzo Miniero <lminiero@gmail.com>
2018-09-13 14:45:45 +0200

    Added experimental support for mDNS candidates

--- sdp.c
+++ sdp.c
@@ -16,10 +16,13 @@
  * \ref protocols
  */
 
+#include <netdb.h>
+
 #include "janus.h"
 #include "ice.h"
 #include "sdp.h"
 #include "utils.h"
+#include "ip-utils.h"
 #include "debug.h"
 #include "events.h"
 
@@ -592,11 +595,25 @@ int janus_sdp_parse_candidate(void *ice_stream, const char *candidate, int trick
 		}
 	}
 	if(res >= 7) {
-		/* FIXME Discard mDNS candidates for now: in the future, we may want to try and resolve them
-		 * https://tools.ietf.org/html/draft-ietf-rtcweb-mdns-ice-candidates-00 */
 		if(strstr(rip, ".local")) {
-			JANUS_LOG(LOG_WARN, "[%"SCNu64"] Ignoring unsupported mDNS candidate\n", handle->handle_id);
-			return res;
+			/* The IP is actually an mDNS address, try to resolve it
+			 * https://tools.ietf.org/html/draft-ietf-rtcweb-mdns-ice-candidates-00 */
+			struct addrinfo *info = NULL;
+			janus_network_address addr;
+			janus_network_address_string_buffer addr_buf;
+			if(getaddrinfo(rip, NULL, NULL, &info) != 0 ||
+					janus_network_address_from_sockaddr(info->ai_addr, &addr) != 0 ||
+					janus_network_address_to_string_buffer(&addr, &addr_buf) != 0) {
+				JANUS_LOG(LOG_WARN, "[%"SCNu64"] Couldn't resolve mDNS address (%s), dropping candidate\n",
+					handle->handle_id, rip);
+				if(info)
+					freeaddrinfo(info);
+				return res;
+			}
+			freeaddrinfo(info);
+			JANUS_LOG(LOG_WARN, "[%"SCNu64"] mDNS address (%s) resolved: %s\n",
+				handle->handle_id, rip, janus_network_address_string_from_buffer(&addr_buf));
+			g_strlcpy(rip, janus_network_address_string_from_buffer(&addr_buf), sizeof(rip));
 		}
 		/* Add remote candidate */
 		component = stream->component;
