5f725572b92e43f6200e460e570abdff6047c373
Piasy <xz4215@gmail.com>
2018-10-09 19:54:54 +0800

    print RTP header extension type in uppercase hex
    It's more human readable in uppercase hex string, because it's uppercase hex string in RFC.

--- postprocessing/janus-pp-rec.c
+++ postprocessing/janus-pp-rec.c
@@ -511,7 +511,7 @@ int main(int argc, char *argv[])
 		rotation = -1;
 		if(rtp->extension) {
 			janus_pp_rtp_header_extension *ext = (janus_pp_rtp_header_extension *)(prebuffer+12+skip);
-			JANUS_LOG(LOG_VERB, "  -- -- RTP extension (type=%"SCNu16", length=%"SCNu16")\n",
+			JANUS_LOG(LOG_VERB, "  -- -- RTP extension (type=0x%"PRIX16", length=%"SCNu16")\n",
 				ntohs(ext->type), ntohs(ext->length));
 			skip += 4 + ntohs(ext->length)*4;
 			if(audio_level_extmap_id > 0)
