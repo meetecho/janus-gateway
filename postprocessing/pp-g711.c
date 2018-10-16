92b7c43cce1fff798515e776b9d579a1614911e7
Lorenzo Miniero <lminiero@gmail.com>
2018-10-16 11:33:25 +0200

    Support for a couple of RTP extensions in the postprocessor

--- postprocessing/pp-g711.c
+++ postprocessing/pp-g711.c
@@ -4,7 +4,7 @@
  * \brief    Post-processing to generate .wav files (headers)
  * \details  Implementation of the post-processing code needed to
  * generate raw .wav files out of G.711 (mu-law or a-law) RTP frames.
- * 
+ *
  * \ingroup postprocessing
  * \ref postprocessing
  */
@@ -184,6 +184,9 @@ int janus_pp_g711_process(FILE *file, janus_pp_frame_packet *list, int *working)
 			JANUS_LOG(LOG_WARN, "Dropping previously marked audio packet (time ~%"SCNu64"s)\n", (tmp->ts-list->ts)/48000);
 			tmp = tmp->next;
 			continue;
+		}
+		if(tmp->audiolevel != -1) {
+			JANUS_LOG(LOG_VERB, "Audio level: %d dB\n", tmp->audiolevel);
 		}
 		guint16 diff = tmp->prev == NULL ? 1 : (tmp->seq - tmp->prev->seq);
 		len = 0;
