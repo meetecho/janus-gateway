db455c20d81c641e2fdd22741ca484bbe0e9478c
Lorenzo Miniero <lminiero@gmail.com>
2018-12-04 13:10:33 +0100

    Added sample gstreamer script to feed multistream mountpoint

---
+++ plugins/streams/test_gstreamer1_multistream.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+gst-launch-1.0 \
+  audiotestsrc ! \
+    audioresample ! audio/x-raw,channels=1,rate=16000 ! \
+    opusenc bitrate=20000 ! \
+      rtpopuspay ! udpsink host=127.0.0.1 port=5102 \
+  videotestsrc pattern=ball ! \
+    video/x-raw,width=320,height=240,framerate=15/1 ! \
+    videoscale ! videorate ! videoconvert ! timeoverlay ! \
+    vp8enc error-resilient=1 ! \
+      rtpvp8pay ! udpsink host=127.0.0.1 port=5104 \
+  videotestsrc ! \
+    video/x-raw,width=320,height=240,framerate=15/1 ! \
+    videoscale ! videorate ! videoconvert ! timeoverlay ! \
+    vp8enc error-resilient=1 ! \
+      rtpvp8pay ! udpsink host=127.0.0.1 port=5106
