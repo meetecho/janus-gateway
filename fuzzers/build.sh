d604f9f44773b77625f6a4674ea6b05b6a7237a9
Alessandro Toppi <atoppi@meetecho.com>
2019-01-17 14:20:57 +0000

    Rename CC to FUZZ_CC.

--- fuzzers/build.sh
+++ fuzzers/build.sh
@@ -7,7 +7,7 @@ ENV=${ENV-"local"}
 SRC=${SRC-"$(dirname $(dirname $(pwd)))"}
 OUT=${OUT-"$(pwd)/out"}
 WORK=${WORK-"$(pwd)"}
-CC=${CC-"clang"}
+FUZZ_CC=${CC-"clang"}
 FUZZ_CFLAGS=""
 FUZZ_LDFLAGS=""
 FUZZ_CCLD=""
@@ -22,12 +22,12 @@ elif [ "$ENV" == "local" ]; then
 	# Default configuration: use address and undefined behaviour sanitizers
 	FUZZ_CFLAGS=${CFLAGS-"-O1 -fno-omit-frame-pointer -g -ggdb3 -fsanitize=address,undefined -fsanitize-address-use-after-scope"}
 	FUZZ_LDFLAGS=${LDFLAGS-"-O1 -fno-omit-frame-pointer -g -ggdb3 -fsanitize=address,undefined -fsanitize-address-use-after-scope"}
-	if [[ $CC == clang* ]]; then
+	if [[ $FUZZ_CC == clang* ]]; then
 		# For coverage testing with clang uncomment
 		# FUZZ_CFLAGS="-O1 -fno-omit-frame-pointer -g -ggdb3 -fprofile-instr-generate -fcoverage-mapping"
 		# FUZZ_LDFLAGS="-O1 -fno-omit-frame-pointer -g -ggdb3 -fprofile-instr-generate -fcoverage-mapping"
 
-		# Enable clang libFuzzer: add it to CFLAGS and link against
+		# When clang is detected always use libFuzzer but link against
 		# libFuzzer only if LIB_FUZZING_ENGINE has not been defined
 		FUZZ_CFLAGS="$FUZZ_CFLAGS -fsanitize=fuzzer-no-link"
 		if [ ! -z $FUZZ_ENGINE ]; then
@@ -36,7 +36,7 @@ elif [ "$ENV" == "local" ]; then
 			FUZZ_LDFLAGS="$FUZZ_LDFLAGS -fsanitize=fuzzer"
 		fi
 	fi
-	FUZZ_CCLD=$CC
+	FUZZ_CCLD=$FUZZ_CC
 fi
 
 rm -f $WORK/*.a $WORK/*.o
@@ -44,7 +44,7 @@ rm -f $WORK/*.a $WORK/*.o
 # Build and archive necessary Janus objects
 cd $SRC/janus-gateway
 ./autogen.sh
-./configure CC="$CC" CFLAGS="$FUZZ_CFLAGS" --disable-docs --disable-post-processing --disable-turn-rest-api --disable-all-transports --disable-all-plugins --disable-all-handlers
+./configure CC="$FUZZ_CC" CFLAGS="$FUZZ_CFLAGS" --disable-docs --disable-post-processing --disable-turn-rest-api --disable-all-transports --disable-all-plugins --disable-all-handlers
 make clean
 JANUS_OBJECTS="janus-log.o janus-utils.o janus-rtcp.o"
 make -j$(nproc) $JANUS_OBJECTS
@@ -61,7 +61,7 @@ engines=$(find $SRC/janus-gateway/fuzzers/ -name "*standalone.c")
 for sourceFile in $engines; do
   name=$(basename $sourceFile .c)
   echo "Building engine: $name"
-  $CC -c $FUZZ_CFLAGS $sourceFile -o $WORK/$name.o
+  $FUZZ_CC -c $FUZZ_CFLAGS $sourceFile -o $WORK/$name.o
 done
 
 # Build Fuzzers
@@ -71,7 +71,7 @@ for sourceFile in $fuzzers; do
   name=$(basename $sourceFile .c)
   echo "Building fuzzer: $name"
 
-  $CC -c $FUZZ_CFLAGS $DEPS_CFLAGS -I. -I$SRC/janus-gateway $sourceFile -o $WORK/$name.o
+  $FUZZ_CC -c $FUZZ_CFLAGS $DEPS_CFLAGS -I. -I$SRC/janus-gateway $sourceFile -o $WORK/$name.o
   $FUZZ_CCLD $FUZZ_LDFLAGS $WORK/${name}.o -o $OUT/${name} $FUZZ_ENGINE $JANUS_LIB $DEPS_LIB
   
   if [ -d "$SRC/janus-gateway/fuzzers/corpora/${name}" ]; then
