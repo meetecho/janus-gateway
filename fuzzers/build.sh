14bcb07f6697c0b9cba9cc130c3916260a9f0bdd
Alessandro Toppi <atoppi@meetecho.com>
2019-02-11 18:14:20 +0100

    Add config.sh script, simplify a lot build.sh.

--- fuzzers/build.sh
+++ fuzzers/build.sh
@@ -1,60 +1,69 @@
 #!/bin/bash -eu
 
-ENV=${ENV-"local"}
+# Load script configuration
+source config.sh
 
-# Try to get these vars from the environment
+# Set fuzzing environment
+# Fallback to local
+FUZZ_ENV=${FUZZ_ENV-$DEFAULT_ENV}
+
+# Set working paths from the environment
 # Fallback to values used for local testing
-SRC=${SRC-"$(dirname $(dirname $(pwd)))"}
-OUT=${OUT-"$(pwd)/out"}
-WORK=${WORK-"$(pwd)"}
-FUZZ_CC=${CC-"clang"}
-FUZZ_CFLAGS=""
-FUZZ_LDFLAGS=""
-FUZZ_CCLD=""
+SRC=${SRC-$DEFAULT_SRC}
+OUT=${OUT-$DEFAULT_OUT}
+WORK=${WORK-$DEFAULT_WORK}
+
+# Set compiler from the environment
+# Fallback to clang
+FUZZ_CC=${CC-$DEFAULT_CC}
+
+# Set linker from the environment (CXX is used as linker in oss-fuzz)
+# Fallback to clang
+FUZZ_CCLD=${CXX-$DEFAULT_CCLD}
+
+# Set CFLAGS from the environment
+# Fallback to using address and undefined behaviour sanitizers
+FUZZ_CFLAGS=${CFLAGS-$DEFAULT_CFLAGS}
+
+# Set LDFLAGS from the environment (CXXFLAGS var is used for linker flags in oss-fuzz)
+# Fallback to using address and undefined behaviour sanitizers
+FUZZ_LDFLAGS=${CXXFLAGS-${LDFLAGS-$DEFAULT_LDFLAGS}}
+
+# Set fuzzing engine from the environment (optional)
 FUZZ_ENGINE=${LIB_FUZZING_ENGINE-""}
 
-if [ "$ENV" == "oss-fuzz" ]; then
-	# CXX and CXXFLAGS are used as linker and linker flags in oss-fuzz
-	FUZZ_CFLAGS=$CFLAGS
-	FUZZ_LDFLAGS=$CXXFLAGS
-	FUZZ_CCLD=$CXX
-elif [ "$ENV" == "local" ]; then
-	# Default configuration: use address and undefined behaviour sanitizers
-	FUZZ_CFLAGS=${CFLAGS-"-O1 -fno-omit-frame-pointer -g -ggdb3 -fsanitize=address,undefined -fsanitize-address-use-after-scope -fno-sanitize-recover=undefined -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION"}
-	FUZZ_LDFLAGS=${LDFLAGS-"-O1 -fno-omit-frame-pointer -g -ggdb3 -fsanitize=address,undefined -fno-sanitize-recover=undefined -fsanitize-address-use-after-scope"}
-	if [[ $FUZZ_CC == clang* ]]; then
-		# For coverage testing with clang uncomment
-		# FUZZ_CFLAGS="-O1 -fno-omit-frame-pointer -g -ggdb3 -fprofile-instr-generate -fcoverage-mapping -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION"
-		# FUZZ_LDFLAGS="-O1 -fno-omit-frame-pointer -g -ggdb3 -fprofile-instr-generate -fcoverage-mapping"
+# Mess with the flags only in local execution
+if [[ $FUZZ_ENV == "local" &&  $FUZZ_CC == clang* ]]; then
+	# For coverage testing with clang uncomment
+	# 	FUZZ_CFLAGS="$COVERAGE_CFLAGS"
+	# 	FUZZ_LDFLAGS="$COVERAGE_LDFLAGS"
 
-		# When clang is detected always use libFuzzer but link against
-		# libFuzzer only if LIB_FUZZING_ENGINE has not been defined
+	# Add fuzzer CFLAG only if not present
+	if [[ ! $FUZZ_CFLAGS =~ .*-fsanitize=([^\s].*)*fuzzer(-.*)* ]]; then
 		FUZZ_CFLAGS="$FUZZ_CFLAGS -fsanitize=fuzzer-no-link"
-		if [ ! -z $FUZZ_ENGINE ]; then
+	fi
+	# Add fuzzer LDFLAG only if not present
+	if [[ ! $FUZZ_LDFLAGS =~ .*-fsanitize=([^\s].*)*fuzzer(-.*)* ]]; then
+		# Link against libFuzzer only if FUZZ_ENGINE has not been set
+		if [[ ! -z $FUZZ_ENGINE ]]; then
 			FUZZ_LDFLAGS="$FUZZ_LDFLAGS -fsanitize=fuzzer-no-link"
 		else
 			FUZZ_LDFLAGS="$FUZZ_LDFLAGS -fsanitize=fuzzer"
 		fi
 	fi
-	FUZZ_CCLD=$FUZZ_CC
 fi
 
 rm -f $WORK/*.a $WORK/*.o
 
 # Build and archive necessary Janus objects
+JANUS_LIB="$WORK/janus-lib.a"
 cd $SRC/janus-gateway
 ./autogen.sh
-./configure CC="$FUZZ_CC" CFLAGS="$FUZZ_CFLAGS" --disable-docs --disable-post-processing --disable-turn-rest-api --disable-all-transports --disable-all-plugins --disable-all-handlers
+./configure CC="$FUZZ_CC" CFLAGS="$FUZZ_CFLAGS" $JANUS_CONF_FLAGS
 make clean
-JANUS_OBJECTS="janus-log.o janus-utils.o janus-rtcp.o janus-rtp.o"
 make -j$(nproc) $JANUS_OBJECTS
-JANUS_LIB="$WORK/janus-lib.a"
 ar rcs $JANUS_LIB $JANUS_OBJECTS
 cd -
-
-# Fetch dependencies
-DEPS_CFLAGS="$(pkg-config --cflags glib-2.0)"
-DEPS_LIB="-Wl,-Bstatic $(pkg-config --libs glib-2.0) -pthread -Wl,-Bdynamic"
 
 # Build standalone fuzzing engines
 engines=$(find $SRC/janus-gateway/fuzzers/engines/ -name "*.c")
