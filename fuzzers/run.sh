93186203676615bc9e494fb4df99b81bdccd33c1
Alessandro Toppi <atoppi@meetecho.com>
2019-03-26 17:57:29 +0100

    Fuzzer run script: list the files before running with a specified folder, change and move some comments.

--- fuzzers/run.sh
+++ fuzzers/run.sh
@@ -20,44 +20,53 @@ echo "Fuzzer: $TARGET"
 echo "Crash file/folder: $CRASH_FILE"
 echo "Output dir: $OUT"
 
-# run fuzzer
 cd "$OUT"
+
+# Extract the corpus dataset from zipfile
 mkdir -p "$TARGET"_corpus
 mkdir -p "$TARGET"_seed_corpus
 if [ -f "${TARGET}_seed_corpus.zip" ]; then
 	echo "Extracting corpus seed data"
 	unzip -oq "$TARGET"_seed_corpus.zip -d "$TARGET"_seed_corpus
 fi
-# Use -max_len=65535 for network protocols
+
+# Run the target
 if [ -z "$CRASH_FILE" ]; then
+	# No crash file supplied, start the fuzzer
+	# Use -max_len=65535 for network protocols
 	ASAN_OPTIONS=detect_leaks=1 ./$TARGET -artifact_prefix="./$TARGET-" -print_final_stats=0 -print_corpus_stats=0 -print_coverage=0 -jobs=${JOBS} -workers=${WORKERS} "$TARGET"_corpus "$TARGET"_seed_corpus
 	# tail -f fuzz*.log
 elif [ -f "$CRASH_FILE" ]; then
-	# rerun to reproduce with a supplied crash file
+	# Run without fuzzing to reproduce a bug with a supplied crash file
 	ASAN_OPTIONS=detect_leaks=1 ./$TARGET $CRASH_FILE
-	# rerun with GDB to reproduce and debug
+	# Rerun with GDB to reproduce and debug
 	#ASAN_OPTIONS=abort_on_error=1 gdb --args ./$TARGET $CRASH_FILE
 elif [ -d "$CRASH_FILE" ]; then
-	# run without fuzzing, with an user supplied folder
-	ASAN_OPTIONS=detect_leaks=1 ./$TARGET "$CRASH_FILE"/*
+	# Run without fuzzing, with an user supplied crashes folder
+	files=$(find "$CRASH_FILE" -maxdepth 1 -type f)
+	if [ -z $files ]; then
+		echo "Empty crashes folder specified!"
+		exit 1
+	fi
+	ASAN_OPTIONS=detect_leaks=1 ./$TARGET $files
 else
 	echo "Invalid crash file/folder specified!"
 	exit 1
 fi
 
-# run without fuzzing, using the extracted corpus dataset (regression testing)
+# Run without fuzzing, using the extracted corpus dataset (regression testing)
 # ASAN_OPTIONS=detect_leaks=1 ./$TARGET "$TARGET"_seed_corpus/*
 
-# run fuzzer for coverage testing
+# Run the target for coverage testing
 # NAME="$TARGET".$(date +%s)
 # LLVM_PROFILE_FILE="$NAME".profraw ./$TARGET "$TARGET"_seed_corpus/*
 # llvm-profdata merge -sparse "$NAME".profraw -o "$NAME".profdata
 # llvm-cov show "$TARGET" -instr-profile="$NAME".profdata "$SRC"/rtcp.c "$SRC"/rtp.c "$SRC"/utils.c -use-color -format=html > "$NAME".html
 
 # dump crashing pattern
-# hexdump -C "$TARGET"-crash-458003b01372ea8ae6456f86da40d3b1d32d905d
+# hexdump -C "$CRASH_FILE"
 
 # Convert to pcap
-# od -Ax -tx1 -v crash-458003b01372ea8ae6456f86da40d3b1d32d905d > crash.hex
-# text2pcap -u1000,2000 crash.hex crash.pcap
+# od -Ax -tx1 -v "$CRASH_FILE" > "$CRASH_FILE".hex
+# text2pcap -u1000,2000 "$CRASH_FILE".hex "$CRASH_FILE".pcap
 
