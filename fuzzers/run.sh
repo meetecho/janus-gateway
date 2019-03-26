3abff27a09dd31bacb555c5f6b30746e0a96a07d
Alessandro Toppi <atoppi@meetecho.com>
2019-03-26 12:24:40 +0100

    Fuzzing run script: use script folder in place of pwd, transform crash file path to an absolute path, detect if the supplied crash file is a file or a folder, rename coverage output files and write them in OUT folder.

--- fuzzers/run.sh
+++ fuzzers/run.sh
@@ -2,15 +2,23 @@
 
 set -eu
 
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
 TARGET=${1:-"rtcp_fuzzer"}
 CRASH_FILE=${2:-""}
+if [[ ! -z "$CRASH_FILE" && "${CRASH_FILE:0:1}" != / && "${CRASH_FILE:0:2}" != ~[/a-z] ]]; then
+	CRASH_FILE="$SCRIPTPATH"/"$CRASH_FILE"
+fi
 HALF_NCORES=$(expr $(nproc) / 2)
 HALF_NCORES=$(($HALF_NCORES > 0 ? $HALF_NCORES : 1))
 JOBS=${JOBS:-${HALF_NCORES}}
 WORKERS=${WORKERS:-${HALF_NCORES}}
-OUT=${OUT:-"$(pwd)/out"}
-SRC=$(dirname $(pwd))
-echo "Executing fuzzer $TARGET"
+OUT=${OUT:-"$SCRIPTPATH/out"}
+SRC=$(dirname $SCRIPTPATH)
+
+echo "Fuzzer: $TARGET"
+echo "Crash file/folder: $CRASH_FILE"
+echo "Output dir: $OUT"
 
 # run fuzzer
 cd "$OUT"
@@ -24,24 +32,30 @@ fi
 if [ -z "$CRASH_FILE" ]; then
 	ASAN_OPTIONS=detect_leaks=1 ./$TARGET -artifact_prefix="./$TARGET-" -print_final_stats=0 -print_corpus_stats=0 -print_coverage=0 -jobs=${JOBS} -workers=${WORKERS} "$TARGET"_corpus "$TARGET"_seed_corpus
 	# tail -f fuzz*.log
-else
+elif [ -f "$CRASH_FILE" ]; then
 	# rerun to reproduce with a supplied crash file
-	./$TARGET $CRASH_FILE
+	ASAN_OPTIONS=detect_leaks=1 ./$TARGET $CRASH_FILE
+	# rerun with GDB to reproduce and debug
+	#ASAN_OPTIONS=abort_on_error=1 gdb --args ./$TARGET $CRASH_FILE
+elif [ -d "$CRASH_FILE" ]; then
+	# run without fuzzing, with an user supplied folder
+	ASAN_OPTIONS=detect_leaks=1 ./$TARGET "$CRASH_FILE"/*
+else
+	echo "Invalid crash file/folder specified!"
+	exit 1
 fi
 
-# run standalone fuzzer or libFuzzer without fuzzing (regression testing)
+# run without fuzzing, using the extracted corpus dataset (regression testing)
 # ASAN_OPTIONS=detect_leaks=1 ./$TARGET "$TARGET"_seed_corpus/*
 
 # run fuzzer for coverage testing
-# LLVM_PROFILE_FILE="$TARGET".profraw ./$TARGET "$TARGET"_seed_corpus/*
-# llvm-profdata merge -sparse "$TARGET".profraw -o "$TARGET".profdata
-# llvm-cov show "$TARGET" -instr-profile="$TARGET".profdata "$SRC"/rtcp.c -use-color -format=html > /tmp/"$TARGET"_coverage.html
+# NAME="$TARGET".$(date +%s)
+# LLVM_PROFILE_FILE="$NAME".profraw ./$TARGET "$TARGET"_seed_corpus/*
+# llvm-profdata merge -sparse "$NAME".profraw -o "$NAME".profdata
+# llvm-cov show "$TARGET" -instr-profile="$NAME".profdata "$SRC"/rtcp.c "$SRC"/rtp.c "$SRC"/utils.c -use-color -format=html > "$NAME".html
 
 # dump crashing pattern
 # hexdump -C "$TARGET"-crash-458003b01372ea8ae6456f86da40d3b1d32d905d
-
-# rerun with GDB to reproduce and debug
-# ASAN_OPTIONS=abort_on_error=1 gdb --args ./$TARGET "$TARGET"-crash-458003b01372ea8ae6456f86da40d3b1d32d905d
 
 # Convert to pcap
 # od -Ax -tx1 -v crash-458003b01372ea8ae6456f86da40d3b1d32d905d > crash.hex
