26a9fe133852c098f1aff8b31001544fadf187b5
Lorenzo Miniero <lminiero@gmail.com>
2019-04-19 15:59:49 +0200

    Removed hardcoded reference to 'janus-gateway' and made it configurable (JANUSGW)

--- fuzzers/build.sh
+++ fuzzers/build.sh
@@ -12,6 +12,7 @@ FUZZ_ENV=${FUZZ_ENV-$DEFAULT_ENV}
 SRC=${SRC-$DEFAULT_SRC}
 OUT=${OUT-$DEFAULT_OUT}
 WORK=${WORK-$DEFAULT_WORK}
+JANUSGW=${JANUSGW-"janus-gateway"}
 
 # Set compiler from the environment
 # Fallback to clang
@@ -63,7 +64,7 @@ rm -f $WORK/*.a $WORK/*.o
 
 # Build and archive necessary Janus objects
 JANUS_LIB="$WORK/janus-lib.a"
-cd $SRC/janus-gateway
+cd $SRC/$JANUSGW
 # Use this variable to skip Janus objects building
 SKIP_JANUS_BUILD=${SKIP_JANUS_BUILD-"0"}
 if [ "$SKIP_JANUS_BUILD" -eq "0" ]; then
@@ -77,7 +78,7 @@ ar rcs $JANUS_LIB $JANUS_OBJECTS
 cd -
 
 # Build standalone fuzzing engines
-engines=$(find $SRC/janus-gateway/fuzzers/engines/ -name "*.c")
+engines=$(find $SRC/$JANUSGW/fuzzers/engines/ -name "*.c")
 for sourceFile in $engines; do
   name=$(basename $sourceFile .c)
   echo "Building engine: $name"
@@ -86,16 +87,16 @@ done
 
 # Build Fuzzers
 mkdir -p $OUT
-fuzzers=$(find $SRC/janus-gateway/fuzzers/ -name "*.c" | grep -v "engines/")
+fuzzers=$(find $SRC/$JANUSGW/fuzzers/ -name "*.c" | grep -v "engines/")
 for sourceFile in $fuzzers; do
   name=$(basename $sourceFile .c)
   echo "Building fuzzer: $name"
 
-  $FUZZ_CC -c $FUZZ_CFLAGS $DEPS_CFLAGS -I. -I$SRC/janus-gateway $sourceFile -o $WORK/$name.o
+  $FUZZ_CC -c $FUZZ_CFLAGS $DEPS_CFLAGS -I. -I$SRC/$JANUSGW $sourceFile -o $WORK/$name.o
   $FUZZ_CCLD $FUZZ_LDFLAGS $WORK/${name}.o -o $OUT/${name} $FUZZ_ENGINE $JANUS_LIB $DEPS_LIB
-  
-  if [ -d "$SRC/janus-gateway/fuzzers/corpora/${name}" ]; then
+
+  if [ -d "$SRC/$JANUSGW/fuzzers/corpora/${name}" ]; then
 	echo "Exporting corpus: $name "
-	zip -jqr --exclude=*LICENSE* $OUT/${name}_seed_corpus.zip $SRC/janus-gateway/fuzzers/corpora/${name}
+	zip -jqr --exclude=*LICENSE* $OUT/${name}_seed_corpus.zip $SRC/$JANUSGW/fuzzers/corpora/${name}
   fi
 done
