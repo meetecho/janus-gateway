45480e05d0dd5484dc676f98561d18f6d9b1d6ef
pawnnail <Irek.Timergazi@gmail.com>
2019-08-07 15:26:20 +1200

    Session media attributes has_audio and has_video are not cleared when call terminates and are carried over into the next call. This sometimes causes the subsequent calls to behave unpredictably. Added code resettings these attributes to janus_sip_media_cleanup().
    Also, analysed other media attributes and found some of them are not cleaned up when
    a call terminates. Additionally, some attributes are not initialiazed when session is
    allocated. Added code to reset media attributes that otherwise are not guaranteed
    to have initial values when a call terminates or when a session is allocated.

--- plugins/janus_sip.c
+++ plugins/janus_sip.c
@@ -1565,6 +1565,22 @@ void janus_sip_create_session(janus_plugin_session *handle, int *error) {
 	session->media.pipefd[0] = -1;
 	session->media.pipefd[1] = -1;
 	session->media.updated = FALSE;
+	session->media.audio_srtp_in = NULL;
+	session->media.audio_srtp_out = NULL;
+	session->media.audio_remote_policy.key = NULL;
+	session->media.audio_remote_policy.ssrc.type = ssrc_any_inbound;
+	session->media.audio_remote_policy.next = NULL;
+	session->media.audio_local_policy.key = NULL;
+	session->media.audio_local_policy.ssrc.type = ssrc_any_inbound;
+	session->media.audio_local_policy.next = NULL;
+	session->media.video_srtp_in = NULL;
+	session->media.video_srtp_out = NULL;
+	session->media.video_remote_policy.key = NULL;
+	session->media.video_remote_policy.ssrc.type = ssrc_any_inbound;
+	session->media.video_remote_policy.next = NULL;
+	session->media.video_local_policy.key = NULL;
+	session->media.video_local_policy.ssrc.type = ssrc_any_inbound;
+	session->media.video_local_policy.next = NULL;
 	janus_mutex_init(&session->rec_mutex);
 	g_atomic_int_set(&session->establishing, 0);
 	g_atomic_int_set(&session->established, 0);
@@ -4490,7 +4506,10 @@ static void janus_sip_media_cleanup(janus_sip_session *session) {
 	}
 	session->media.local_audio_rtp_port = 0;
 	session->media.local_audio_rtcp_port = 0;
+	session->media.remote_audio_rtp_port = 0;
+	session->media.remote_audio_rtcp_port = 0;
 	session->media.audio_ssrc = 0;
+	session->media.audio_ssrc_peer = 0;
 	if(session->media.video_rtp_fd != -1) {
 		close(session->media.video_rtp_fd);
 		session->media.video_rtp_fd = -1;
@@ -4501,7 +4520,10 @@ static void janus_sip_media_cleanup(janus_sip_session *session) {
 	}
 	session->media.local_video_rtp_port = 0;
 	session->media.local_video_rtcp_port = 0;
+	session->media.remote_video_rtp_port = 0;
+	session->media.remote_video_rtcp_port = 0;
 	session->media.video_ssrc = 0;
+	session->media.video_ssrc_peer = 0;
 	session->media.simulcast_ssrc = 0;
 	if(session->media.pipefd[0] > 0) {
 		close(session->media.pipefd[0]);
@@ -4513,6 +4535,31 @@ static void janus_sip_media_cleanup(janus_sip_session *session) {
 	}
 	/* Clean up SRTP stuff, if needed */
 	janus_sip_srtp_cleanup(session);
+
+	// media fields not cleaned up elsewhere
+	g_free(session->media.remote_audio_ip);
+	session->media.remote_audio_ip = NULL;
+	g_free(session->media.remote_video_ip);
+	session->media.remote_video_ip = NULL;
+	session->media.earlymedia = FALSE;
+	session->media.update = FALSE;
+	session->media.updated = FALSE;
+	session->media.autoaccept_reinvites = TRUE;
+	session->media.ready = FALSE;
+	session->media.require_srtp = FALSE;
+	session->media.on_hold = FALSE;
+	session->media.has_audio = FALSE;
+	session->media.audio_pt = -1;
+	session->media.audio_pt_name = NULL;	/* Immutable string, no need to free*/
+	session->media.audio_send = TRUE;
+	session->media.pre_hold_audio_dir = JANUS_SDP_DEFAULT;
+	session->media.has_video = FALSE;
+	session->media.video_pt = -1;
+	session->media.video_pt_name = NULL;	/* Immutable string, no need to free*/
+	session->media.video_send = TRUE;
+	session->media.pre_hold_video_dir = JANUS_SDP_DEFAULT;
+	janus_rtp_switching_context_reset(&session->media.context);
+
 }
 
 /* Thread to relay RTP/RTCP frames coming from the SIP peer */
@@ -4915,4 +4962,5 @@ gpointer janus_sip_sofia_thread(gpointer user_data) {
 	g_thread_unref(g_thread_self());
 	return NULL;
 }
+
 
