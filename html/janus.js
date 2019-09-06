840c0f2b6762777c5c164de730cfb864dde611ac
Lorenzo Miniero <lminiero@gmail.com>
2019-09-06 13:08:42 +0200

    Improved RTCRtpTransceiver check (see #1759)

--- html/janus.js
+++ html/janus.js
@@ -390,9 +390,14 @@ Janus.init = function(options) {
 				Janus.webRTCAdapter.browserDetails.version < 72) {
 			// Chrome does, but it's only usable from version 72 on
 			Janus.unifiedPlan = false;
-		} else if(!RTCRtpTransceiver || !('currentDirection' in RTCRtpTransceiver.prototype)) {
+		} else if(RTCRtpTransceiver === undefined || RTCRtpTransceiver === null  ||
+				RTCRtpTransceiver.prototype === undefined || RTCRtpTransceiver.prototype === null) {
+			// Some browsers, like "old" Edge, don't have RTCRtpTransceiver at all
+			Janus.unifiedPlan = false;
+		} else if(Janus.webRTCAdapter.browserDetails.browser === 'safari' &&
+				!('currentDirection' in RTCRtpTransceiver.prototype)) {
 			// Safari supports addTransceiver() but not Unified Plan when
-			// currentDirection is not defined (see codepen above)
+			// currentDirection is not defined (see codepen above).
 			Janus.unifiedPlan = false;
 		} else {
 			// Check if addTransceiver() throws an exception
