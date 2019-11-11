a71354b880f64097bf8535dd3afe8e65cd38aa81
Sebastian Schmid <sebastian.j.kummer@gmail.com>
2019-11-11 11:23:09 +0100

    Feature/ignore unreachable ice server (#1854)

--- ice.c
+++ ice.c
@@ -1010,10 +1010,15 @@ int janus_ice_set_stun_server(gchar *stun_server, uint16_t stun_port) {
 
 	/* Test the STUN server */
 	janus_network_address public_addr = { 0 };
-	if(janus_ice_test_stun_server(&addr, janus_stun_port, 0, &public_addr, NULL) < 0)
+	if(janus_ice_test_stun_server(&addr, janus_stun_port, 0, &public_addr, NULL) < 0) {
+		g_free(janus_stun_server);
+		janus_stun_server = NULL;
 		return -1;
+	}
 	if(janus_network_address_to_string_buffer(&public_addr, &addr_buf) != 0) {
 		JANUS_LOG(LOG_ERR, "Could not resolve public address...\n");
+		g_free(janus_stun_server);
+		janus_stun_server = NULL;
 		return -1;
 	}
 	const char *public_ip = janus_network_address_string_from_buffer(&addr_buf);
