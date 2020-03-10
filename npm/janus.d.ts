e2e0a321ee3e784963e5d4c251870c7a8778f8ee
Paul Zhang <pszhang92@gmail.com>
2020-03-10 14:27:08 -0500

    Fix typo in ContructorOptions interface

--- npm/janus.d.ts
+++ npm/janus.d.ts
@@ -23,7 +23,7 @@ declare namespace JanusJS {
 		dependencies?: Dependencies;
 	}
 
-	interface ConstuctorOptions {
+	interface ConstructorOptions {
 		server: string | string[];
 		iceServers?: RTCIceServer[];
 		ipv6?: boolean;
@@ -138,7 +138,7 @@ declare namespace JanusJS {
 		static error(...args: any[]): void;
 		static randomString(length: number): string;
 
-		constructor(options: ConstuctorOptions);
+		constructor(options: ConstructorOptions);
 
 		getServer(): string;
 		isConnected(): boolean;
