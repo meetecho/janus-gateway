cd8e53a590e9d3cfa421503de21caf99c938da01
Alexander Clark <agclark@technolutions.com>
2020-04-02 15:22:35 -0400

    additional constraints for screenshare media
    adding support for existing screenshareFrameRate property as well as new screenshareHeight and screenshareWidth properties in getDisplayMedia

--- html/janus.js
+++ html/janus.js
@@ -2182,7 +2182,11 @@ function Janus(gatewayCallbacks) {
 						// The new experimental getDisplayMedia API is available, let's use that
 						// https://groups.google.com/forum/#!topic/discuss-webrtc/Uf0SrR4uxzk
 						// https://webrtchacks.com/chrome-screensharing-getdisplaymedia/
-						navigator.mediaDevices.getDisplayMedia({ video: true, audio: media.captureDesktopAudio })
+						constraints.video = { frameRate: media.screenshareFrameRate };
+						if (media.screenshareHeight) constraints.video.height = media.screenshareHeight;
+						if (media.screenshareWidth) constraints.video.width = media.screenshareWidth;
+						constraints.audio = media.captureDesktopAudio;
+						navigator.mediaDevices.getDisplayMedia(constraints)
 							.then(function(stream) {
 								pluginHandle.consentDialog(false);
 								if(isAudioSendEnabled(media) && !media.keepAudio) {
