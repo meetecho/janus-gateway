1e65c82ec280d9263fc6788e3fa2a67aa61d0995
Lorenzo Miniero <lminiero@gmail.com>
2020-05-08 09:26:01 +0200

    Fixed typo in AudioBridge error response

--- plugins/janus_audiobridge.c
+++ plugins/janus_audiobridge.c
@@ -4447,7 +4447,7 @@ admin_response:
 			if(!response) {
 				/* Prepare JSON error event */
 				response = json_object();
-				json_object_set_new(response, "streaming", json_string("event"));
+				json_object_set_new(response, "audiobridge", json_string("event"));
 				json_object_set_new(response, "error_code", json_integer(error_code));
 				json_object_set_new(response, "error", json_string(error_cause));
 			}
