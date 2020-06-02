0bb11f33d6c143d05ffae79796e46962019f5621
Lionel Nicolas <lionelnicolas@users.noreply.github.com>
2020-06-02 09:46:34 -0400

    make libcurl follow RTSP 302 redirections (#2195)

--- plugins/janus_streaming.c
+++ plugins/janus_streaming.c
@@ -6260,6 +6260,7 @@ static int janus_streaming_rtsp_connect_to_server(janus_streaming_mountpoint *mp
 	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
 	curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 5L);
 	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 0L);
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 	/* Any authentication to take into account? */
 	if(source->rtsp_username && source->rtsp_password) {
 		/* Point out that digest authentication is only available is libcurl >= 7.45.0 */
