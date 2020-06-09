495b5b1447f3c904d4697fdd349318795e6aa8ad
Glandos <bugs-github@antipoul.fr>
2020-06-09 10:49:29 +0200

    Update janus.d.ts (#2202)
    add types for `attachMediaStream` and `reattachMediaStream` helpers.

--- npm/janus.d.ts
+++ npm/janus.d.ts
@@ -137,6 +137,8 @@ declare namespace JanusJS {
 		static warn(...args: any[]): void;
 		static error(...args: any[]): void;
 		static randomString(length: number): string;
+		static attachMediaStream(element: HTMLMediaElement, stream: MediaStream);
+		static reattachMediaStream(to: HTMLMediaElement, from: HTMLMediaElement);
 
 		constructor(options: ConstructorOptions);
 
