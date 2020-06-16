085ed393e8be15834e8b263a990494986518bd05
Lorenzo Miniero <lminiero@gmail.com>
2020-06-16 11:44:18 +0200

    Removed unused variable

--- plugins/janus_nosip.c
+++ plugins/janus_nosip.c
@@ -2423,7 +2423,6 @@ static void *janus_nosip_relay_thread(void *data) {
 						bytes = buflen;
 					}
 					/* Check if the SSRC changed (e.g., after a re-INVITE or UPDATE) */
-					guint32 timestamp = ntohl(header->timestamp);
 					janus_rtp_header_update(header, &session->media.context, video, 0);
 					/* Save the frame if we're recording */
 					janus_recorder_save_frame(video ? session->vrc_peer : session->arc_peer, buffer, bytes);
