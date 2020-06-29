de8cc8d5880fbb6a1518b7f6809a2117c95db25b
Neil Kinnish <hello@neilkinnish.com>
2020-06-29 09:16:37 +0100

    Removed count from janus_pp_opus_process

--- postprocessing/pp-opus.h
+++ postprocessing/pp-opus.h
@@ -17,7 +17,7 @@
 #include "pp-rtp.h"
 
 int janus_pp_opus_create(char *destination, char *metadata);
-int janus_pp_opus_process(FILE *file, janus_pp_frame_packet *list, int *working, uint32_t count);
+int janus_pp_opus_process(FILE *file, janus_pp_frame_packet *list, int *working);
 void janus_pp_opus_close(void);
 
 #endif
