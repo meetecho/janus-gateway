e415329395bdd8b95454a1dc2309a6004faf6fec
serhii mamedov <serhiy.ma@starkey.co.il>
2020-07-21 14:31:16 +0300

    indentation fix

--- plugins/duktape/state.js
+++ plugins/duktape/state.js
@@ -5,40 +5,43 @@ var rooms = {};
 var managerSessions = {};
 
 function getRoom(roomId) {
-	var room = null;
-	if (rooms[roomId]) {
-		room = rooms[roomId];
-	} else {
-		// new room template
-		var newRoomTemplate = { roomId: 0, roomName: "", managerSessionID: 0, publishers: [], sessions: [] };
+  var room = null;
+  if (rooms[roomId]) {
+    room = rooms[roomId];
+  } else {
+    // new room template
+    var newRoomTemplate = { roomId: 0, roomName: "", managerSessionID: 0, publishers: [], sessions: [] };
 
-		room = newRoomTemplate;
-		room.roomId = roomId
-		rooms[roomId] = room;
-	}
-	return room
+    room = newRoomTemplate;
+    room.roomId = roomId
+    rooms[roomId] = room;
+  }
+  return room
 }
+
 function getSession(sessionID) {
-	var session = null;
-	if (sessions[sessionID]) {
-		session = sessions[sessionID];
-	} else {
-		// Objects Templates
-		var newSessionTemplate = { id: 0, janusServer: janusServer, room: 0, subscribers: [], publishers: [], isConnected: false };
-		// new session template
-		session = newSessionTemplate;
-		session.id = sessionID;
-		sessions[sessionID] = session;
-		console.log("New session was burn !!!! ", sessionID, session)
-	}
-	return session
+  var session = null;
+  if (sessions[sessionID]) {
+    session = sessions[sessionID];
+  } else {
+    // Objects Templates
+    var newSessionTemplate = { id: 0, janusServer: janusServer, room: 0, subscribers: [], publishers: [], isConnected: false };
+    // new session template
+    session = newSessionTemplate;
+    session.id = sessionID;
+    sessions[sessionID] = session;
+    console.log("New session (" + sessionID + ") was burn !!!! ", session);
+  }
+  return session
 }
+
 function setSession(session) {
-	sessions[session.id] = session;
-	console.log("session was update  !!!! ", session.id, session)
+  sessions[session.id] = session;
+  console.log("session (" + session.id + ") was updated!!!! ", session);
 }
+
 function setRoom(room) {
-	rooms[room.roomId] = room;
+  rooms[room.roomId] = room;
 }
 
 module.exports = {
