3c84cb8069fa450050677e1a53ce95618b384258
neilyoung <neilyoung@users.noreply.github.com>
2020-08-31 13:18:01 +0200

    Updates according to comments

--- html/videoroomtest.js
+++ html/videoroomtest.js
@@ -64,7 +64,7 @@ var bitrateTimer = [];
 
 var doSimulcast = (getQueryStringValue("simulcast") === "yes" || getQueryStringValue("simulcast") === "true");
 var doSimulcast2 = (getQueryStringValue("simulcast2") === "yes" || getQueryStringValue("simulcast2") === "true");
-var subscriber_mode = (getQueryStringValue("subscriber-mode") === "yes")
+var subscriber_mode = (getQueryStringValue("subscriber-mode") === "yes")  || getQueryStringValue("subscriber-mode") === "true");
 
 $(document).ready(function() {
 	// Initialize the library (all console debuggers enabled)
@@ -163,12 +163,13 @@ $(document).ready(function() {
 											myid = msg["id"];
 											mypvtid = msg["private_id"];
 											Janus.log("Successfully joined room " + msg["room"] + " with ID " + myid);
-											if (subscriber_mode) {
+											if(subscriber_mode) {
                                                 						$('#videojoin').hide();
                                                 						$('#videos').removeClass('hide').show();
                                             						}
-                                            						else
+                                            						else {
                                                 						publishOwnFeed(true);
+											}
 
 											// Any new feed to attach to?
 											if(msg["publishers"]) {
