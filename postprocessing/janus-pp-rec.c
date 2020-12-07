018f8b91b126a420c333ded4779630213830af93
ricardo-salgado-tekever <74801629+ricardo-salgado-tekever@users.noreply.github.com>
2020-12-07 10:12:19 +0000

    Corrected janus pp rec (#2472)
    Corrected data recordings packet header read (using fread with a size > 1 bytes returns the number of elements read, not bytes)

--- postprocessing/janus-pp-rec.c
+++ postprocessing/janus-pp-rec.c
@@ -671,8 +671,8 @@ int main(int argc, char *argv[])
 		if(data) {
 			/* Things are simpler for data, no reordering is needed: start by the data time */
 			gint64 when = 0;
-			bytes = fread(&when, sizeof(gint64), 1, file);
-			if(bytes < (int)sizeof(gint64)) {
+			bytes = fread(&when, 1, sizeof(gint64), file);
+			if(bytes < sizeof(gint64)) {
 				JANUS_LOG(LOG_WARN, "Missing data timestamp header");
 				break;
 			}
