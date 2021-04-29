0ec7ab0b775bdf055c122466b57494350377d72f
Lorenzo Miniero <lminiero@gmail.com>
2021-04-29 18:17:11 +0200

    Fixed duplicate video in multiopus demo

--- html/multiopus.js
+++ html/multiopus.js
@@ -52,8 +52,7 @@ var janus = null;
 var echotest = null;
 var opaqueId = "multiopus-"+Janus.randomString(12);
 
-var localTracks = {}, localVideos = 0,
-	remoteTracks = {}, remoteVideos = 0;
+var remoteTracks = {}, remoteVideos = 0;
 var bitrateTimer = null;
 var spinner = null;
 
@@ -245,71 +244,7 @@ $(document).ready(function() {
 									}
 								},
 								onlocaltrack: function(track, on) {
-									Janus.debug("Local track " + (on ? "added" : "removed") + ":", track);
-									// We use the track ID as name of the element, but it may contain invalid characters
-									var trackId = track.id.replace(/[{}]/g, "");
-									if(!on) {
-										// Track removed, get rid of the stream and the rendering
-										var stream = localTracks[trackId];
-										if(stream) {
-											try {
-												var tracks = stream.getTracks();
-												for(var i in tracks) {
-													var mst = tracks[i];
-													if(mst)
-														mst.stop();
-												}
-											} catch(e) {}
-										}
-										if(track.kind === "video") {
-											$('#myvideo' + trackId).remove();
-											localVideos--;
-											if(localVideos === 0) {
-												// No video, at least for now: show a placeholder
-												if($('#videoleft .no-video-container').length === 0) {
-													$('#videoleft').append(
-														'<div class="no-video-container">' +
-															'<i class="fa fa-video-camera fa-5 no-video-icon"></i>' +
-															'<span class="no-video-text">No webcam available</span>' +
-														'</div>');
-												}
-											}
-										}
-										delete localTracks[trackId];
-										return;
-									}
-									// If we're here, a new track was added
-									var stream = localTracks[trackId];
-									if(stream) {
-										// We've been here already
-										return;
-									}
-									if($('#videoleft video').length === 0) {
-										$('#videos').removeClass('hide').show();
-									}
-									if(track.kind === "audio") {
-										// We ignore local audio tracks, they'd generate echo anyway
-										if(localVideos === 0) {
-											// No video, at least for now: show a placeholder
-											if($('#videoleft .no-video-container').length === 0) {
-												$('#videoleft').append(
-													'<div class="no-video-container">' +
-														'<i class="fa fa-video-camera fa-5 no-video-icon"></i>' +
-														'<span class="no-video-text">No webcam available</span>' +
-													'</div>');
-											}
-										}
-									} else {
-										// New video track: create a stream out of it
-										localVideos++;
-										$('#videoleft .no-video-container').remove();
-										stream = new MediaStream();
-										stream.addTrack(track.clone());
-										localTracks[trackId] = stream;
-										Janus.log("Created local stream:", stream);
-										$('#videoleft').append('<video class="rounded centered" id="myvideo' + trackId + '" width="100%" height="100%" autoplay playsinline muted="muted"/>');
-										Janus.attachMediaStream($('#myvideo' + trackId).get(0), stream);
-									}
+									// We ignore the stream we got here, we're using the static video to render it
 									if(echotest.webrtcStuff.pc.iceConnectionState !== "completed" &&
 											echotest.webrtcStuff.pc.iceConnectionState !== "connected") {
 										$("#videoleft").parent().block({
