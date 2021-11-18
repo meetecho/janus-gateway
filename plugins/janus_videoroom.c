02757f5addcdfd339ee7cf2b12d49015f2f66ac2
Bartosz Balazinski <bartosz.balazinski@nuance.com>
2021-11-18 17:11:15 -0500

    periodic

--- plugins/janus_videoroom.c
+++ plugins/janus_videoroom.c
@@ -6140,8 +6140,6 @@ static void *janus_videoroom_handler(void *data) {
 			JANUS_VALIDATE_JSON_OBJECT(root, join_parameters,
 				error_code, error_cause, TRUE,
 				JANUS_VIDEOROOM_ERROR_MISSING_ELEMENT, JANUS_VIDEOROOM_ERROR_INVALID_ELEMENT);
-			JANUS_LOG(LOG_INFO, "Request type '%s'", request_text);
-			janus_check_param_checksum(root, "join");
 			if(error_code != 0)
 				goto error;
 			janus_mutex_lock(&rooms_mutex);
@@ -6155,6 +6153,8 @@ static void *janus_videoroom_handler(void *data) {
 			janus_mutex_lock(&videoroom->mutex);
 			json_t *ptype = json_object_get(root, "ptype");
 			const char *ptype_text = json_string_value(ptype);
+			JANUS_LOG(LOG_INFO, "Request type '%s', ptype '%s'\n", request_text, ptype_text);
+			janus_check_param_checksum(root, "join");
 			if(!strcasecmp(ptype_text, "publisher")) {
 				JANUS_LOG(LOG_VERB, "Configuring new publisher\n");
 				JANUS_VALIDATE_JSON_OBJECT(root, publisher_parameters,
