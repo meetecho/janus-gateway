82dd27837c8f09aab82f23424412252bd1d0e0fc
David Engelmaier <David.Engelmaier@doclerholding.com>
2022-03-28 16:44:31 +0200

    Set MTU size to play stream in NordVPN/Chrome incognito mode setup

--- src/plugins/streams/test_gstreamer1.sh
+++ src/plugins/streams/test_gstreamer1.sh
@@ -7,5 +7,5 @@ gst-launch-1.0 \
   videotestsrc ! \
     video/x-raw,width=320,height=240,framerate=15/1 ! \
     videoscale ! videorate ! videoconvert ! timeoverlay ! \
-    vp8enc error-resilient=1 ! \
-      rtpvp8pay ! udpsink host=127.0.0.1 port=5004
+    vp8enc keyframe-max-dist=7 error-resilient=1 ! \
+      rtpvp8pay mtu=1360 ! udpsink host=127.0.0.1 port=5004
