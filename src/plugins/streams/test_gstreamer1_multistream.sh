4df2b52a09ce008277470f6efdc8f7dc8f6e0e8b
David Engelmaier <David.Engelmaier@doclerholding.com>
2022-03-28 17:11:39 +0200

    Set MTU size only at 1200, loose the keyframe interval setting

--- src/plugins/streams/test_gstreamer1_multistream.sh
+++ src/plugins/streams/test_gstreamer1_multistream.sh
@@ -12,5 +12,5 @@ gst-launch-1.0 \
   videotestsrc ! \
     video/x-raw,width=320,height=240,framerate=15/1 ! \
     videoscale ! videorate ! videoconvert ! timeoverlay ! \
-    vp8enc keyframe-max-dist=7 error-resilient=1 ! \
-      rtpvp8pay mtu=1360 ! udpsink host=127.0.0.1 port=5106
+    vp8enc error-resilient=1 ! \
+      rtpvp8pay mtu=1200 ! udpsink host=127.0.0.1 port=5106
