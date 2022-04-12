1dbe2baea19290853bdb2454210da1fc248f8484
aelezovic <adnan.elezovic@infobip.com>
2022-04-12 17:14:52 +0200

    store description under "d" in json header

--- src/record.c
+++ src/record.c
@@ -350,7 +350,7 @@ int janus_recorder_save_frame(janus_recorder *recorder, char *buffer, uint lengt
 		if(recorder->fmtp)
 			json_object_set_new(info, "f", json_string(recorder->fmtp));				/* Codec-specific info */
 		if(recorder->description)
-			json_object_set_new(info, "desc", json_string(recorder->description));		/* Stream description */
+			json_object_set_new(info, "d", json_string(recorder->description));		/* Stream description */
 		if(recorder->extensions) {
 			/* Add the extmaps to the JSON object */
 			json_t *extmaps = NULL;
