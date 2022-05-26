53425370ab54c8c406490ed16e5fe44184fac4dd
Lorenzo Miniero <lminiero@gmail.com>
2022-05-26 14:30:45 +0200

    Fixed exception when adding helper in SIP plugin demo

--- html/siptest.js
+++ html/siptest.js
@@ -953,6 +953,7 @@ function doHangup(ev) {
 // works exactly as the default one: you can add more than one "helper", and
 // obviously the more you have, the more concurrent calls you can have.
 function addHelper(helperCreated) {
+	helperCreated = (typeof helperCreated == "function") ? helperCreated : Janus.noop;
 	helpersCount++;
 	var helperId = helpersCount;
 	helpers[helperId] = { id: helperId };
