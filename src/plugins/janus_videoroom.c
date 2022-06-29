1a51174a3cefabe793365d72e7521e8cef36c4a3
aelezovic <adnan.elezovic@infobip.com>
2022-06-29 16:00:42 +0200

    enable recording on session start

--- src/plugins/janus_videoroom.c
+++ src/plugins/janus_videoroom.c
@@ -6866,6 +6866,19 @@ void janus_videoroom_setup_media(janus_plugin_session *handle) {
 			/* Notify all other participants that there's a new boy in town */
 			janus_videoroom_notify_about_publisher(participant, FALSE);
 			janus_refcount_decrease(&participant->ref);
+
+			/* Check if we need to start recording */
+			janus_mutex_lock(&participant->rec_mutex);
+			if(participant->room->record) {
+				GList *temp = participant->streams;
+				while(temp) {
+					janus_videoroom_publisher_stream *ps = (janus_videoroom_publisher_stream *)temp->data;
+					janus_videoroom_recorder_create(participant, ps);
+					temp = temp->next;
+				}
+				participant->recording_active = TRUE;
+			}
+			janus_mutex_unlock(&participant->rec_mutex);
 		} else if(session->participant_type == janus_videoroom_p_type_subscriber) {
 			janus_videoroom_subscriber *s = janus_videoroom_session_get_subscriber(session);
 			if(s && s->streams) {
