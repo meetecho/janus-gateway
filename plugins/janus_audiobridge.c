d674b5576d9ca87f60a3bc4a994ee2562f0fd7d2
Lorenzo Miniero <lminiero@gmail.com>
2022-08-02 10:19:28 +0200

    Ignore 'generate_offer' directive when adding plain RTP AudioBridge participants

--- plugins/janus_audiobridge.c
+++ plugins/janus_audiobridge.c
@@ -6403,6 +6403,7 @@ static void *janus_audiobridge_handler(void *data) {
 			participant->reset = FALSE;
 			/* If this is a plain RTP participant, create the socket */
 			if(rtp != NULL) {
+				gen_offer = NULL;
 				const char *ip = json_string_value(json_object_get(rtp, "ip"));
 				uint16_t port = json_integer_value(json_object_get(rtp, "port"));
 				if(participant->codec == JANUS_AUDIOCODEC_OPUS) {
