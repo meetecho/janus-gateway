c9b06659b75e694573e0449dcf927decd3aabffc
Lorenzo Miniero <lminiero@gmail.com>
2023-02-09 15:24:54 +0100

    Bump plugin API version, due to new extension support

--- src/plugins/plugin.h
+++ src/plugins/plugin.h
@@ -171,7 +171,7 @@ janus_plugin *create(void) {
  * Janus instance or it will crash.
  *
  */
-#define JANUS_PLUGIN_API_VERSION	102
+#define JANUS_PLUGIN_API_VERSION	103
 
 /*! \brief Initialization of all plugin properties to NULL
  *
