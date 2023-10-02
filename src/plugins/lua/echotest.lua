db92fff186d2816505d36364d70a8c1595139e9f
Benson Muite <bkmgit@users.noreply.github.com>
2023-10-02 15:45:38 +0300

    Typo fix and style consistency

--- src/plugins/lua/echotest.lua
+++ src/plugins/lua/echotest.lua
@@ -7,9 +7,9 @@ json = require('json')
 -- We also import our own SDP helper utilities: you may have better ones
 sdp = require('janus-sdp')
 -- Let's also use our ugly stdout logger just for the fun of it: to add
--- some color to the text we use the lua-trm library
+-- some color to the text we use the lua-term library
 -- (https://github.com/hoelzro/lua-term)
-colors = require "term.colors"
+colors = require('term.colors')
 logger = require('janus-logger')
 
 -- Example details
