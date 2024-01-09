6cb51bfb0536efcc34c61d1c5ac04d8d3cc1c4fb
CorthiClem <clement.corthier@ubble.ai>
2024-01-09 19:38:33 +0100

    Removed useless filename

--- src/postprocessing/pp-av1.c
+++ src/postprocessing/pp-av1.c
@@ -55,9 +55,7 @@ int janus_pp_av1_create(char *destination, char *metadata, gboolean faststart, c
 		return -1;
 	}
 
-	char filename[1024];
-	snprintf(filename, sizeof(filename), "%s", destination);
-	fctx->url = g_strdup(filename);
+	fctx->url = g_strdup(destination);
 
 	vStream = janus_pp_new_video_avstream(fctx, AV_CODEC_ID_AV1, max_width, max_height);
 	if(vStream == NULL) {
