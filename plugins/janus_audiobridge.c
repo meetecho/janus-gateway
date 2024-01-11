bf06c21806b5ab2681fe9859edf60b5f31e4d496
Lorenzo Miniero <lminiero@gmail.com>
2024-01-11 11:21:25 +0100

    Fixed memory leak in AudioBridge in case of late packets

--- plugins/janus_audiobridge.c
+++ plugins/janus_audiobridge.c
@@ -1077,6 +1077,7 @@ room-<unique room ID>: {
 #include <ogg/ogg.h>
 #endif
 #include <speex/speex_jitter.h>
+#define GE32(a,b) (((spx_int32_t)((a)-(b)))>=0)
 
 #include <arpa/inet.h>
 #include <net/if.h>
@@ -5845,7 +5846,11 @@ void janus_audiobridge_incoming_rtp(janus_plugin_session *handle, janus_plugin_r
 			jbp.len = 0;
 			jbp.timestamp = ntohl(rtp->timestamp);
 			jbp.span = (participant->codec == JANUS_AUDIOCODEC_OPUS ? 960 : 160);
-			jitter_buffer_put(participant->jitter, &jbp);
+			if(GE32(jbp.timestamp + jbp.span + jbp.span, jitter_buffer_get_pointer_timestamp(participant->jitter))) {
+				jitter_buffer_put(participant->jitter, &jbp);
+			} else {
+				janus_audiobridge_buffer_packet_destroy(pkt);
+			}
 			janus_mutex_unlock(&participant->qmutex);
 		}
 	}
