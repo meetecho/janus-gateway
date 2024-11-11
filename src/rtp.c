19b1d1bb93890bdc7d9017597191513eacf3ea99
Nati Baratz <nati.baratz@kaltura.com>
2024-11-11 13:29:01 +0200

    Fixed log format specifier

--- src/rtp.c
+++ src/rtp.c
@@ -1232,7 +1232,7 @@ gboolean janus_rtp_simulcasting_context_process_rtp(janus_rtp_simulcasting_conte
 						context->substream_target_temp = 0;
 					if(context->substream_target_temp != prev_target) {
 
-						JANUS_LOG(LOG_WARN, "No packet received on substream %d for %d Milliseconds, falling back to %d\n",
+						JANUS_LOG(LOG_WARN, "No packet received on substream %d for %"SCNi64" Milliseconds, falling back to %d\n",
 							context->substream, (delayMicrosec / 1000), context->substream_target_temp);
 
 						/* Notify the caller that we (still) need a PLI */
