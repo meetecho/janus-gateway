5a93caff093e1d9632ffdafd528165c14417f63f
val.v.petrov <val.v.petrov@tinkoff.ru>
2025-11-06 11:12:48 +0300

    License info

--- src/plugins/janus_audiobridge.c
+++ src/plugins/janus_audiobridge.c
@@ -1201,6 +1201,51 @@ room-<unique room ID>: {
  * request on the participant handle, which will finalize the media
  * establishment.
  *
+ * \subsection ablimiter AudioBridge limiter
+ *
+ * The AudioBridge plugin has a built-in audio limiter, which can be used
+ * to limit the volume of the audio stream coming to participants.
+ * This limiter is disabled by default, and can be enabled by setting
+ * the \c use_limiter property to \c true in the \c create request.
+ * The limiter is applied to recording (if there are 2 or more participants
+ * speaking) and to all tracks that participants receive from the
+ * AudioBridge plugin (if there are 2 or mote participants speaking besides
+ * this participant).
+ *
+ * The limiter contains code parts adapted from the WebRTC code which is
+ * licensed under the BSD license:
+\verbatim
+Copyright (c) 2011, The WebRTC project authors. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+  * Neither the name of Google nor the names of its contributors may
+    be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+\endverbatim
+ *
  */
 
 #include "plugin.h"
