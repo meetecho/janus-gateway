d0f6bdd46a7f4c3557942ab4450be918d8d62531
Oleksandr Mihal <oleksandr.mihal@zultys.com>
2025-11-26 12:36:45 +0200

    Prevent 'use-after-free' in SIP session helpers (#3613)

--- src/plugins/janus_sip.c
+++ src/plugins/janus_sip.c
@@ -2462,6 +2462,9 @@ void janus_sip_destroy_session(janus_plugin_session *handle, int *error) {
 				janus_sip_session *helper = (janus_sip_session *)temp->data;
 				if(helper != NULL && helper->handle != NULL) {
 					/* Get rid of this helper */
+					helper->helper = FALSE;
+					helper->master_id = 0;
+					helper->master = NULL;
 					janus_refcount_decrease(&session->ref);
 					janus_refcount_decrease(&helper->ref);
 					gateway->end_session(helper->handle);
@@ -2476,6 +2479,9 @@ void janus_sip_destroy_session(janus_plugin_session *handle, int *error) {
 			gboolean found = (g_list_find(master->helpers, session) != NULL);
 			if(found) {
 				master->helpers = g_list_remove(master->helpers, session);
+				session->helper = FALSE;
+				session->master_id = 0;
+				session->master = NULL;
 				janus_refcount_decrease(&session->ref);
 				janus_refcount_decrease(&master->ref);
 			}
@@ -3483,7 +3489,7 @@ static void *janus_sip_handler(void *data) {
 				janus_mutex_unlock(&master->mutex);
 				session->helper = FALSE;
 				session->master = NULL;
-				session->master_id = FALSE;
+				session->master_id = 0;
 				/* Done */
 				session->account.registration_status = janus_sip_registration_status_unregistered;
 				result = json_object();
