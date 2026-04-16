2766a9f9d78732d0a9d0e118d23cc21bd8abe4bb
Lorenzo Miniero <lminiero@gmail.com>
2026-04-16 16:24:34 +0200

    Default JSON serialization to compact in WebTransport plugin

--- src/transports/janus_webtransport.c
+++ src/transports/janus_webtransport.c
@@ -99,7 +99,7 @@ static GHashTable *connections = NULL;
 static janus_mutex wt_mutex = JANUS_MUTEX_INITIALIZER;
 
 /* JSON serialization options */
-static size_t json_format = JSON_INDENT(3) | JSON_PRESERVE_ORDER;
+static size_t json_format = JSON_COMPACT | JSON_PRESERVE_ORDER;
 
 /* Parameter validation (for tweaking and queries via Admin API) */
 static struct janus_json_parameter request_parameters[] = {
@@ -270,8 +270,8 @@ int janus_webtransport_init(janus_transport_callbacks *callback, const char *con
 				/* Compact, so no spaces between separators */
 				json_format = JSON_COMPACT | JSON_PRESERVE_ORDER;
 			} else {
-				JANUS_LOG(LOG_WARN, "Unsupported JSON format option '%s', using default (indented)\n", item->value);
-				json_format = JSON_INDENT(3) | JSON_PRESERVE_ORDER;
+				JANUS_LOG(LOG_WARN, "Unsupported JSON format option '%s', using default (compact)\n", item->value);
+				json_format = JSON_COMPACT | JSON_PRESERVE_ORDER;
 			}
 		}
 
